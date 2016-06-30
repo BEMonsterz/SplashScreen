65ed1ff2e0da5aafc0182854459d982837e19461
Bryan Ayllon <bryanayllonroa@gmail.com>
2016-06-29 19:25:05 -0500

    Might of Worked

--- SplashScreen/ViewController.m
+++ SplashScreen/ViewController.m
@@ -29,10 +29,11 @@
         [[NSUserDefaults standardUserDefaults] setValue:@"1" forKey:@"aValue"];
         [[NSUserDefaults standardUserDefaults] synchronize];
     splash = [[UIView alloc] initWithFrame:CGRectMake(0,0 ,self.view.frame.size.width,self.view.frame.size.height)];
-    splash.backgroundColor = [UIColor yellowColor];
-    [self.view addSubview:splash];
-    
-        
+        //rgb(66, 165, 245)
+    splash.backgroundColor = [UIColor colorWithRed:66/255.0 green:165/255.0 blue:245/255.0 alpha:1.0];
+       
+        [self.view addSubview:splash];
+
         
     splash.hidden = NO;
     splash.alpha = 1.0f;
@@ -45,7 +46,18 @@
         splash.hidden = YES;
     }];
         
+        //        welcomeScreen = [[UILabel alloc]initWithFrame:CGRectMake(177, 313, 125, 40 )];
 
+        welcomeScreen = [[UILabel alloc]initWithFrame:CGRectMake(145, 313, 125, 40 )];
+        
+        [welcomeScreen setTextColor:[UIColor whiteColor]];
+        
+        [welcomeScreen setText:@"Welcome!"];
+        [welcomeScreen setFont:[UIFont systemFontOfSize:20]];
+        
+
+        
+ [splash addSubview:welcomeScreen];
 }
 }
 
@@ -58,18 +70,28 @@
 
         
         
-        splash.hidden = NO;
-        splash.alpha = 1.0f;
+        
+        
+        
+        
+
+        
+
+        
+        welcomeScreen.hidden = NO;
+        welcomeScreen.alpha = 1.0f;
         // Then fades it away after 2 seconds (the cross-fade animation will take 0.5s)
         [UIView animateWithDuration:-20.0 delay:-2.0 options:0 animations:^{
             // Animate the alpha value of your imageView from 1.0 to 0.0 here
             splash.alpha = 0.0f;
         } completion:^(BOOL finished) {
             // Once the animation is completed and the alpha has gone to 0.0, hide the view for good
-            splash.hidden = YES;
+            welcomeScreen.hidden = YES;
         }];
         
- 
+        
+        
+
     
     
     
